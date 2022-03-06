e759392fc28d0ca8b17d1faa32677933ef35546d
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-06 11:45:20 +0100

    task:question2 and 3

--- background Assignment/helper/createBytes.js
+++ background Assignment/helper/createBytes.js
@@ -3,7 +3,7 @@ const ethers = require('ethers');
 async function createBytes(args) {
     const name = args[0];
     const bytes = ethers.utils.formatBytes32String(name);
-    console.log("Bytes: ", bytes);
+    console.log("Bytes: \"" + bytes + "\"");
 };
 
 createBytes(process.argv.slice(2))
