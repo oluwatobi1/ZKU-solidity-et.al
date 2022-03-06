668366552c080b224e8d3fe0119634ea6d84bb4c
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-06 11:07:59 +0100

    nothing

--- background Assignment/helper/createBytes.js
+++ background Assignment/helper/createBytes.js
@@ -4,6 +4,6 @@ async function createBytes(args) {
     const name = args[0];
     const bytes = ethers.utils.formatBytes32String(name);
     console.log("Bytes: ", bytes);
-}
+};
 
 createBytes(process.argv.slice(2))
--- background Assignment/helper/parseBytes.js
+++ background Assignment/helper/parseBytes.js
@@ -4,6 +4,6 @@ async function parseBytes(args) {
     const bytes = args[0];
     const name = ethers.utils.parseBytes32String(bytes);
     console.log("Name: ", name);
-}
+};
 
 parseBytes(process.argv.slice(2))
