538309f12403063b9f1f2a5df861c2ddaeac86b0
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-07 17:36:54 +0100

    adds code with comment

--- background Assignment/HelloWorld.sol
+++ background Assignment/HelloWorld.sol
@@ -1,2 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.7.1;
+
+contract HelloWorld {
+    // initialize variable helloworldNumber
+    // variable is set to 0 by default
+    uint256 helloWorldNumber = 7650;
+
+    // this function return the latest value of helloWorldNumber
+    // it reads of the value of network
+    function retrieve() public view returns(uint256){
+        return helloWorldNumber;
+    }
+    
+    // Extra feature:this function can be used to change the value of helloWorldNumber
+    function update(uint256 _helloWorldNumber) public {
+        helloWorldNumber = _helloWorldNumber;
+    }
+}
